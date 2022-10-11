64f77c69244056c190392f6b8646f750cb05892a
BabatundeOye <108598015+BabatundeOye@users.noreply.github.com>
2022-10-10 18:56:45 -0600

    Update two-tier.tf

--- two-tier.tf
+++ two-tier.tf
@@ -251,7 +251,7 @@ resource "aws_instance" "terraform_web_server_2" {
 
   user_data = <<EOF
 		  #!/bin/bash
-      apt-get update
+                  apt-get update
 		  apt-get install -y apache2
 		  systemctl start apache2
 		  systemctl enable apache2
