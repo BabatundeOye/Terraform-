5301a043ffd769b948a49100290836119ee616c8
BabatundeOye <teamsoldd@gmail.com>
2022-10-24 10:19:13 +0000

    practice_terraform_modules

---
+++ practice_terraform_4/ec2-instance/main.tf
@@ -0,0 +1,11 @@
+#-----ec2-instance/main.tf
+
+resource "aws_instance" "web_server" {
+  ami           = var.ami_id
+  instance_type = var.instance
+
+  tags = {
+    Name = "my_web_server"
+  }
+}
+
---
+++ practice_terraform_4/ec2-instance/output.tf
@@ -0,0 +1,9 @@
+#-----ec2-instance/output.tf
+
+output "public_ip" {
+  value = aws_instance.web_server.public_ip
+}
+
+output "ec2_tags" {
+  value = aws_instance.web_server.tags_all.Name
+}
