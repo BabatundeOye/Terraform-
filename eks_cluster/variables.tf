dda309a27c24366c9973bb0011cf0cc34a78c46f
BabatundeOye <teamsoldd@gmail.com>
2022-11-17 10:07:56 +0000

    eks_cluster

---
+++ eks_cluster/variables.tf
@@ -0,0 +1,5 @@
+#root/variable.tf
+variable "aws_region" {
+  type    = string
+  default = "us-west-2"
+}
---
+++ eks_cluster/vpc/variables.tf
@@ -0,0 +1,29 @@
+#---eks/variables.tf
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "aws_region" {
+  default = "us-west-2"
+}
+
+variable "public_cidrs" {
+  type = list(any)
+}
+
+variable "private_cidrs" {
+  type = list(any)
+}
+
+variable "public_sn_count" {
+  type = number
+}
+
+variable "private_sn_count" {
+  type = number
+}
+
+variable "max_subnets" {
+  type = number
+}
